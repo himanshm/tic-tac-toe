a63806bc8c9380118540e8d62a809e93f766c2d5
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-11 16:30:00 +0530

    Render a symbol on board by updating object state immutably

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 type CellValue = 'X' | 'O' | null;
 
 const initialGameBoard: CellValue[][] = [
@@ -7,14 +9,28 @@ const initialGameBoard: CellValue[][] = [
 ];
 
 function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex: number, colIndex: number) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      console.log(updatedBoard);
+      updatedBoard[rowIndex][colIndex] = 'X';
+      return updatedBoard;
+    });
+  }
   return (
     <ol id='game-board'>
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
