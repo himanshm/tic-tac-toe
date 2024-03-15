8685ea758fdb7845ed4e3ef6d730fbdcfc7a43f2
Himanshu Mishra <sai.himanshum011@gmail.com>
2024-03-15 18:28:42 +0530

    Remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import GameBoard from './components/GameBoard';
 import Player from './components/Player';
 import Log from './components/Log';
-// import { WINNING_COMBINATIONS } from './util/winning-combinations';
 import GameOver from './components/GameOver';
 import {
   Players,
@@ -12,83 +11,6 @@ import {
   deriveGameBoard,
   deriveWinner,
 } from './util/helpers';
-
-// interface Players {
-//   X: string;
-//   O: string;
-// }
-
-// const PLAYERS: Players = {
-//   X: 'Player 1',
-//   O: 'Player 2',
-// };
-
-// const INITIAL_GAMING_BOARD: CellValue[][] = [
-//   [null, null, null],
-//   [null, null, null],
-//   [null, null, null],
-// ];
-
-// export type CellValue = 'X' | 'O' | null;
-
-// export type Turn = {
-//   square: {
-//     row: number;
-//     col: number;
-//   };
-//   player: CellValue;
-// };
-
-// function deriveActivePlayer(gameTurns: Turn[]): CellValue {
-//   let currentPlayer: CellValue = 'X';
-
-//   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-//     currentPlayer = 'O';
-//   }
-
-//   return currentPlayer;
-// }
-
-// function deriveWinner(
-//   gameBoard: CellValue[][],
-//   players: Players
-// ): string | null {
-//   let winner: string | null = null;
-
-//   for (const combination of WINNING_COMBINATIONS) {
-//     const firstSquareSymbol =
-//       gameBoard[combination[0].row][combination[0].column];
-
-//     const secondSquareSymbol =
-//       gameBoard[combination[1].row][combination[1].column];
-
-//     const thirdSquareSymbol =
-//       gameBoard[combination[2].row][combination[2].column];
-
-//     if (
-//       firstSquareSymbol &&
-//       firstSquareSymbol === secondSquareSymbol &&
-//       firstSquareSymbol === thirdSquareSymbol
-//     ) {
-//       winner = players[firstSquareSymbol];
-//     }
-//   }
-
-//   return winner;
-// }
-
-// function deriveGameBoard(turns: Turn[]) {
-//   const gameBoard = [...INITIAL_GAMING_BOARD.map((array) => [...array])];
-
-//   for (const turn of turns) {
-//     const { square, player } = turn;
-//     const { row, col } = square;
-
-//     gameBoard[row][col] = player;
-//   }
-
-//   return gameBoard;
-// }
 
 function App() {
   const [players, setPlayers] = useState<Players>(PLAYERS);
